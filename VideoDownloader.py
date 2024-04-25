a91a62dea6821ccb360e528d78073c2d3a669998
Vinicius Lopes <47784023+TheVino@users.noreply.github.com>
2024-04-25 05:24:22 -0300

    Update VideoDownloader.py
    Fixed some function calls and display order.
    Added thumbnail display to the program.

--- VideoDownloader.py
+++ VideoDownloader.py
@@ -1,13 +1,10 @@
 # CTK Repo:     https://customtkinter.tomschimansky.com/
 # Pytube Repo:  https://pytube.io/en/latest/api.html#pytube.YouTube.thumbnail_url
 
-import tkinter, customtkinter, urllib.request, io
+import tkinter, urllib.request, io
+import customtkinter as ctk
 from pytube import YouTube, Playlist
 from PIL import ImageTk, Image
-
-# Var
-ctk = customtkinter
-
 
 # fix this based on this tutorial
 def DisplayThumbnail(thumb_url):
@@ -15,20 +12,22 @@ def DisplayThumbnail(thumb_url):
         raw_data = u.read()
     image = Image.open(io.BytesIO(raw_data))
     photo = ImageTk.PhotoImage(image)
-    thumb = ctk.CTkLabel(app, image="")
-    thumb.configure(image=photo)
-    return image, photo
+    thumb = ctk.CTkLabel(app,text="", image=photo)
+    thumb.image = photo
+    thumb.pack(fill="both", expand=True)
+    return thumb
 
 def StartDownload():
     try: 
         ytLink  = link.get()
         ytObject= YouTube(ytLink, on_progress_callback = on_progress)
-        video   = ytObject.streams.get_highest_resolution()
-        # ytObject.streams -> use this to add video resolutions for download
+        video   = ytObject.streams.get_highest_resolution()       # ytObject.streams -> use this to add video resolutions for download
         title.configure(text=ytObject.title, text_color="white")
-        thumbnail = thumb.configure(text=ytObject.thumbnail_url, text_color="blue")
-        thumb_url = ytObject.thumbnail_url
-        print(thumb_url)
+        if hasattr(app, 'thumb'):   # Destroy the previous thumbnail label if it exists
+            app.thumb.destroy()
+        thumb = DisplayThumbnail(ytObject.thumbnail_url)  # Update thumbnail here
+        # thumb.pack()  # Pack the thumbnail label
+        app.thumb = thumb  # Store the reference to the new thumbnail label
         video.download()
         print("Download Complete")
         finishLabel.configure(text="Downloaded!", text_color="green")
@@ -48,16 +47,13 @@ def on_progress(stream, chunk, bytes_remaining):
     # Update Progress Bar
     progressBar.set(float(percentage_of_completion)/100)
 
-
-# Playslist download function already made. Need to test and add a selector for implementation
+# Playlist download function already made. Need to test and add a selector for implementation
 def Playlist():
     ytLink=link.get()
     ytObject= Playlist(ytLink, on_progress_callback = on_progress)
     for video in ytObject.videos:
         video.streams.first().download()
     return
-
-
 
 # System Settings
 ctk.set_appearance_mode("System")
@@ -80,11 +76,6 @@ link.pack()
 # Finished Downloading
 finishLabel = ctk.CTkLabel(app, text="")
 finishLabel.pack()
-
-# Thumbnail
-DisplayThumbnail("https://i.ytimg.com/vi/iO9L4QA1Phw/hq720.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYACpAWKAgwIABABGGUgTyhDMA8=&rs=AOn4CLC1Rzk_VGFMomiy5dTgnRpgglImJQ")
-thumb = ctk.CTkLabel(app, image="")
-thumb.pack()
 
 # Progress percentage
 pPercentage = ctk.CTkLabel(app, text="0%")
